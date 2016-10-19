b6fdfd5fc4b6941509d1d22b70433bc9ef6a8c2c
kompjoefriek <roel@kompjoefriek.nl>
2016-10-19 02:08:49 +0200

    Tests -> tests

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,8 +4,8 @@ import pytest
 from simr.Configuration.Configuration import Configuration
 
 def test_simple_config():
-    Configuration("Tests/simple_config.xml")
+    Configuration("tests/simple_config.xml")
 
 def test_cyclic_config():
     with pytest.raises(RuntimeWarning):
-        Configuration("Tests/cyclic_config.xml")
+        Configuration("tests/cyclic_config.xml")
