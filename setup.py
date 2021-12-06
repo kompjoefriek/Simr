ae258b66f6c0ffec6c8b03ebf8488d08e552a98c
KompjoeFriek <kompjoefriek@users.noreply.github.com>
2021-12-06 03:00:45 +0100

    Add Github Actions
    Create unittests.yml
    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "Simr",
+    version = "0.0.6",
+    author = "Sander Krause, Roel van Nuland",
+    author_email = "sanderkrause@gmail.com, roel@kompjoefriek.nl",
+    description = ("Simr is a simulation runner written in Python 3"),
+    license = "MIT",
+    keywords = "taskrunner",
+    url = "https://github.com/kompjoefriek/Simr",
+    packages=['simr', 'tests'],
+    install_requires=[
+        'psutil',
+        'windows-curses ; platform_system=="Windows"'
+    ]
+)
